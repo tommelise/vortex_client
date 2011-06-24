0f02844bd0dd02a92247306ab58a2f2f282dd9fb
Lise Hamre <lise@usit.uio.no>
2011-06-24 09:40:40 +0200

    Alter tags with add or replace

--- examples/tags.rb
+++ examples/tags.rb
@@ -2,10 +2,6 @@
 #
 # Author: Lise Hamre & Thomas Flemming
 
-#  tags = Tags.new("https://vortex-dav.uio.no/brukere/lise/test/")
-#  keywords = {"word1" => "new_word1", "word2" => "new_word2"}
-#  tags.search_replace(path, keywords)
-
 require 'rubygems'
 require 'vortex_client'
 require 'json'
@@ -16,53 +12,70 @@ class Tags
     @vortex = Vortex::Connection.new(host,:use_osx_keychain => true)
   end
 
-  # TODO Make this more robust:
-  #  - return [] if no tags
   def tags(path)
     doc = @vortex.get(path)
     data =  JSON.parse(doc)
-    return data["properties"]["tags"]
+    if(data["properties"]["tags"])
+      return data["properties"]["tags"]
+    else
+      return []
+    end
   end
 
-  def add(path, new_tags)
-    tags_on_server = tags(path)
-    doc = @vortex.get(path)
-    data =  JSON.parse(doc)
-    tags_on_server += new_tags
-    data["properties"]["tags"] = tags_on_server.uniq
-    @vortex.put_string(path, data.to_json  )
+  def add(tags_on_server, new_tags)
+    if(tags_on_server)then
+      return tags_on_server += new_tags
+    else
+      return tags_on_server = new_tags
+    end
   end
 
-  def search_replace(path, tags)
+  def search_replace(tags_on_server, tags)
+    if(tags_on_server)then
+      tags.each do |tag, new_tag|
+        if(tags_on_server.include?(tag))then
+          tags_on_server[tags_on_server.index(tag)]=new_tag
+          tags_on_server.flatten
+         puts tag.to_s + " => " + new_tag.to_s 
+       end
+      end
+    end
+     return tags_on_server
+  end
+
+  def alter_tags(path,tags,alteration)
     @vortex.find(path,:recursive => true,:filename=>/\.html$/) do |item|
       # puts item.uri.to_s
-      
       data = nil
       begin
         data = JSON.parse(item.content)
       rescue
         puts "Warning. Bad document. Not json:" + item.uri.to_s
       end
-
       if(data)then
-        tags_on_server = data["properties"]["tags"]
-        # puts "keywords: " + tags_on_server.join(", ")
-        tags.each do |tag, new_tag|
-          #puts "tag" + tag.to_s + new_tag.to_s 
-          if(tags_on_server.grep(tag))then
-            tags_on_server.delete(tag)
-            if(new_tag)then
-              tags_on_server.push(new_tag)
-            end
-          end
+        tags_on_server = data["properties"]["tags"]       
+        if alteration=="add"
+          tags_on_server = add(tags_on_server, tags).uniq
+        elsif alteration=="replace"
+          tags_on_server = search_replace(tags_on_server, tags)
         end
-
         data["properties"]["tags"] = tags_on_server
         item.content = data.to_json
       end
-
-
     end
   end
 
 end
+
+tags = Tags.new("https://www-dav.vortex-demo.uio.no")
+path = "/personer/lise/."
+
+## Replace tags: ##
+keywords = {"word2" => "word6", "word3" => "word5"}
+#tags.alter_tags(path, keywords, "replace")
+
+## Add tags: ##
+new_tags = ["new-tag1", "new-tag2"]
+tags.alter_tags(path, new_tags, "add")
+
+
