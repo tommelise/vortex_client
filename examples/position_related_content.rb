6620cacfb86678315291cf8852883618b07e1aa6
Lise Hamre <lise@usit.uio.no>
2011-06-29 12:33:16 +0200

    added def image_size and wide_image to position_related_content.rb

--- examples/position_related_content.rb
+++ examples/position_related_content.rb
@@ -1,5 +1,8 @@
 # related_content
-#
+# This program moves related content from right to bottom or vice versa
+# Value: false_if_content: If related content has contents, you can choose to leave it at the right hand side
+# def image_width can control the position movement based on the size of the immage.
+# def image_size kan resize images to fit better at wide or narrow body-with (NOT TESTED)
 # Author: Lise Hamre
 
 require 'rubygems'
@@ -22,9 +25,71 @@ class Position_related_content
     end
   end
 
-  def position_related_content(path,value)
+  def parse_css(css)
+    css_hash = { }
+    css.split(/;/).each do |item|
+      item = item.split(/:/)
+      css_hash[ item[0].gsub(/\s/,'') ] = item[1].gsub(/\s/,'').gsub("px","")
+    end
+    return css_hash
+  end
+  
+  
+  def image_size(data,filename,new_width)
+    content = data["properties"]["content"]
+    if content !=nil
+      html = Nokogiri::HTML.parse(content)
+      if html != nil
+        html.css("img").each do |image|
+          style = image.attr("style")
+          if style != nil
+            dim = parse_css(style)
+            if dim["width"].to_i > 500 && dim["width"].to_i < new_width
+              puts filename
+              #          puts "width: " + dim["width"].to_s + " height: " + dim["height"].to_s
+              new_height = (new_width/(dim["width"].to_f))*dim["height"].to_f
+              new_style = "width:  " + new_width.to_s + "; height: " + new_height.round.to_s + ";"
+              #          puts "New_style: " + new_style 
+              html = html.to_s.gsub(style,new_style)
+            end
+          end
+        end
+      end
+      content = html.to_s.gsub("<html>","").gsub("</html>","").gsub("<body>","").gsub("</body>","")
+      #      puts "content: " + content
+      data["properties"]["content"] = content
+    end
+    return data
+  end
+  
+  
+  def wide_image(data, filename, width)
+    wide_image = false
+    content = data["properties"]["content"]
+    if content !=nil
+      html = Nokogiri::HTML.parse(content)
+      if html != nil
+        html.css("img").each do |image|
+          style = image.attr("style")
+           if style != nil
+             dim = parse_css(style)
+             if dim["width"].to_i > width
+               puts filename
+               puts "width: " + dim["width"].to_s + " height: " + dim["height"].to_s
+               wide_image = true
+              end
+           end
+        end
+      end
+    end
+    return wide_image
+  end
+  
+
+  def position_related_content(path,value,img_width)
     @vortex.find(path,:recursive => true,:filename=>/\.html$/) do |item|
-      puts item.uri.to_s
+#      puts item.uri.to_s
+      rel_cont_bottom = value
       data = nil
       begin
         data = JSON.parse(item.content)
@@ -38,8 +103,12 @@ class Position_related_content
           else
             rel_cont_bottom = "true"
           end
-        else rel_cont_bottom = value
         end
+#        data = image_size(data,item.uri.to_s,img_width) 
+#        if wide_image(data,item.uri,img_width)
+#          rel_cont_bottom = false
+#        end
+#        puts "related content at bottom: " + rel_cont_bottom.to_s
         data["properties"]["hideAdditionalContent"] = rel_cont_bottom
         item.content = data.to_json
       end
@@ -51,10 +120,10 @@ end
 
 position_related_content = Position_related_content.new("https://www-dav.vortex-demo.uio.no")
 path = "/personer/lise/."
-#positionrelated_content = Position_related_content.new("https://www-dav.uio.no/")
+
+#position_related_content = Position_related_content.new("https://www-dav.uio.no/")
 #path = "/forskning/tverrfak/culcom/nyheter/."
 
-## send related_content to bottom: (true/false/false_if_content)##
-position_related_content.position_related_content(path, "false_if_content")
-
+## send related_content to bottom: (true/false/false_if_content,img_with)##
+position_related_content.position_related_content(path, "true",500)
 
